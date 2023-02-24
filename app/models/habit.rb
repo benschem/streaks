e9ca42c1e2289a4da55609355e7db96575fb7f65
Ben Schembri <benschembri@gmail.com>
2023-02-24 15:39:57 +1100

    adding some logic from the js version to be converted

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,3 +1,25 @@
 class Habit < ApplicationRecord
+
 end
-#
+
+# JAVASCRIPT CLASS
+# constructor({name, doneToday, streak, record, gap}) {
+#   this.id = Date.now()
+#   this.name = name;
+#   this.doneToday = doneToday || false;
+#   this.streak = streak || 0;
+#   this.record = record || 0;
+#   this.gap = gap || 0;
+# }
+
+# increase(item) {
+#   item++;
+# }
+
+# decrease(item) {
+#   item--;
+# }
+
+# reset(item) {
+#   item = 0;
+# }
